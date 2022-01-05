9f9b19084b01e2f7475305e18dd45ec2a8e01de4
tianze0926 <i@tianze.me>
2022-01-05 23:06:25 +0800

    fix segmatation fault

--- src/LexParser/MyVisitor.h
+++ src/LexParser/MyVisitor.h
@@ -1,114 +1,207 @@
 #pragma once
 
-#include "antlr4-runtime.h"
-#include "SQLVisitor.h"
-#include "SQLParser.h"
-
 #include <cstdio>
 
+#include "SQLParser.h"
+#include "SQLVisitor.h"
+#include "antlr4-runtime.h"
+
 class MyVisitor : public SQLVisitor {
-public:
-	MyVisitor(){
+   public:
+    MyVisitor() {
+    }
+    antlrcpp::Any visitProgram(SQLParser::ProgramContext *context) {
+		
+        return antlrcpp::Any(0);
+    }
 
-	}
-	antlrcpp::Any visitProgram(SQLParser::ProgramContext *context){
-	}
+    antlrcpp::Any visitStatement(SQLParser::StatementContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitStatement(SQLParser::StatementContext *context){}
+    antlrcpp::Any visitCreate_db(SQLParser::Create_dbContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitCreate_db(SQLParser::Create_dbContext *context){}
+    antlrcpp::Any visitDrop_db(SQLParser::Drop_dbContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitDrop_db(SQLParser::Drop_dbContext *context){}
+    antlrcpp::Any visitShow_dbs(SQLParser::Show_dbsContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitShow_dbs(SQLParser::Show_dbsContext *context){}
+    antlrcpp::Any visitUse_db(SQLParser::Use_dbContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitUse_db(SQLParser::Use_dbContext *context){}
+    antlrcpp::Any visitShow_tables(SQLParser::Show_tablesContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitShow_tables(SQLParser::Show_tablesContext *context){}
+    antlrcpp::Any visitShow_indexes(SQLParser::Show_indexesContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitShow_indexes(SQLParser::Show_indexesContext *context){}
+    antlrcpp::Any visitLoad_data(SQLParser::Load_dataContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitLoad_data(SQLParser::Load_dataContext *context){}
+    antlrcpp::Any visitDump_data(SQLParser::Dump_dataContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitDump_data(SQLParser::Dump_dataContext *context){}
+    antlrcpp::Any visitCreate_table(SQLParser::Create_tableContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitCreate_table(SQLParser::Create_tableContext *context){}
+    antlrcpp::Any visitDrop_table(SQLParser::Drop_tableContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitDrop_table(SQLParser::Drop_tableContext *context){}
+    antlrcpp::Any visitDescribe_table(SQLParser::Describe_tableContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitDescribe_table(SQLParser::Describe_tableContext *context){}
+    antlrcpp::Any visitInsert_into_table(SQLParser::Insert_into_tableContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitInsert_into_table(SQLParser::Insert_into_tableContext *context){}
+    antlrcpp::Any visitDelete_from_table(SQLParser::Delete_from_tableContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitDelete_from_table(SQLParser::Delete_from_tableContext *context){}
+    antlrcpp::Any visitUpdate_table(SQLParser::Update_tableContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitUpdate_table(SQLParser::Update_tableContext *context){}
+    antlrcpp::Any visitSelect_table_(SQLParser::Select_table_Context *context) {
+        printf("1\n");
+		return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitSelect_table_(SQLParser::Select_table_Context *context){
-		printf("1\n");
-	}
+    antlrcpp::Any visitSelect_table(SQLParser::Select_tableContext *context) {
+        printf("2\n");
+		return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitSelect_table(SQLParser::Select_tableContext *context){
-		printf("2\n");
-	}
+    antlrcpp::Any visitAlter_add_index(SQLParser::Alter_add_indexContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitAlter_add_index(SQLParser::Alter_add_indexContext *context){}
+    antlrcpp::Any visitAlter_drop_index(SQLParser::Alter_drop_indexContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitAlter_drop_index(SQLParser::Alter_drop_indexContext *context){}
+    antlrcpp::Any visitAlter_table_drop_pk(SQLParser::Alter_table_drop_pkContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitAlter_table_drop_pk(SQLParser::Alter_table_drop_pkContext *context){}
+    antlrcpp::Any visitAlter_table_drop_foreign_key(SQLParser::Alter_table_drop_foreign_keyContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitAlter_table_drop_foreign_key(SQLParser::Alter_table_drop_foreign_keyContext *context){}
+    antlrcpp::Any visitAlter_table_add_pk(SQLParser::Alter_table_add_pkContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitAlter_table_add_pk(SQLParser::Alter_table_add_pkContext *context){}
+    antlrcpp::Any visitAlter_table_add_foreign_key(SQLParser::Alter_table_add_foreign_keyContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitAlter_table_add_foreign_key(SQLParser::Alter_table_add_foreign_keyContext *context){}
+    antlrcpp::Any visitAlter_table_add_unique(SQLParser::Alter_table_add_uniqueContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitAlter_table_add_unique(SQLParser::Alter_table_add_uniqueContext *context){}
+    antlrcpp::Any visitField_list(SQLParser::Field_listContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitField_list(SQLParser::Field_listContext *context){}
+    antlrcpp::Any visitNormal_field(SQLParser::Normal_fieldContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitNormal_field(SQLParser::Normal_fieldContext *context){}
+    antlrcpp::Any visitPrimary_key_field(SQLParser::Primary_key_fieldContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitPrimary_key_field(SQLParser::Primary_key_fieldContext *context){}
+    antlrcpp::Any visitForeign_key_field(SQLParser::Foreign_key_fieldContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitForeign_key_field(SQLParser::Foreign_key_fieldContext *context){}
+    antlrcpp::Any visitType_(SQLParser::Type_Context *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitType_(SQLParser::Type_Context *context){}
+    antlrcpp::Any visitValue_lists(SQLParser::Value_listsContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitValue_lists(SQLParser::Value_listsContext *context){}
+    antlrcpp::Any visitValue_list(SQLParser::Value_listContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitValue_list(SQLParser::Value_listContext *context){}
+    antlrcpp::Any visitValue(SQLParser::ValueContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitValue(SQLParser::ValueContext *context){}
+    antlrcpp::Any visitWhere_and_clause(SQLParser::Where_and_clauseContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitWhere_and_clause(SQLParser::Where_and_clauseContext *context){}
+    antlrcpp::Any visitWhere_operator_expression(SQLParser::Where_operator_expressionContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitWhere_operator_expression(SQLParser::Where_operator_expressionContext *context){}
+    antlrcpp::Any visitWhere_operator_select(SQLParser::Where_operator_selectContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitWhere_operator_select(SQLParser::Where_operator_selectContext *context){}
+    antlrcpp::Any visitWhere_null(SQLParser::Where_nullContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitWhere_null(SQLParser::Where_nullContext *context){}
+    antlrcpp::Any visitWhere_in_list(SQLParser::Where_in_listContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitWhere_in_list(SQLParser::Where_in_listContext *context){}
+    antlrcpp::Any visitWhere_in_select(SQLParser::Where_in_selectContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitWhere_in_select(SQLParser::Where_in_selectContext *context){}
+    antlrcpp::Any visitWhere_like_string(SQLParser::Where_like_stringContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitWhere_like_string(SQLParser::Where_like_stringContext *context){}
+    antlrcpp::Any visitColumn(SQLParser::ColumnContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitColumn(SQLParser::ColumnContext *context){}
+    antlrcpp::Any visitExpression(SQLParser::ExpressionContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitExpression(SQLParser::ExpressionContext *context){}
+    antlrcpp::Any visitSet_clause(SQLParser::Set_clauseContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitSet_clause(SQLParser::Set_clauseContext *context){}
+    antlrcpp::Any visitSelectors(SQLParser::SelectorsContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitSelectors(SQLParser::SelectorsContext *context){}
+    antlrcpp::Any visitSelector(SQLParser::SelectorContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitSelector(SQLParser::SelectorContext *context){}
+    antlrcpp::Any visitIdentifiers(SQLParser::IdentifiersContext *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitIdentifiers(SQLParser::IdentifiersContext *context){}
+    antlrcpp::Any visitOperator_(SQLParser::Operator_Context *context) {
+        return antlrcpp::Any(0);
+    }
 
-    antlrcpp::Any visitOperator_(SQLParser::Operator_Context *context){}
-
-    antlrcpp::Any visitAggregator(SQLParser::AggregatorContext *context){}
+    antlrcpp::Any visitAggregator(SQLParser::AggregatorContext *context) {
+        return antlrcpp::Any(0);
+    }
 };
