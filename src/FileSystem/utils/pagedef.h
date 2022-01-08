0530cc5895ee013803d61624a0f6406c0291dad1
icr <2504953055@qq.com>
2022-01-08 20:03:00 +0800

    删去typedef int INT

--- src/FileSystem/utils/pagedef.h
+++ src/FileSystem/utils/pagedef.h
@@ -50,7 +50,6 @@ typedef unsigned char uchar;
 typedef unsigned long long ull;
 typedef long long ll;
 typedef double db;
-typedef int INT;
 typedef int(cf)(uchar*, uchar*);
 //int current = 0;
 //int tt = 0;
